af3f8ab4e63b522c8b7bba87084fd6e525b970b4
Khaled Abdelsalam <kh.salama2001@gmail.com>
2023-02-06 17:46:29 +0100

    camera functionality

--- level.py
+++ level.py
@@ -26,8 +26,21 @@ class Level:
                     player = Player((x,y))
                     self.player.add(player)
                     
+    def scroll_map(self):
+        if self.player.sprite.rect.centerx >= (7/8 * SCREEN_WIDTH) and self.player.sprite.direction.x > 0:
+            self.map_shift = -self.player.sprite.player_speed
+            self.player.sprite.direction.x = 0
 
+        elif self.player.sprite.rect.centerx <= (1/8 * SCREEN_WIDTH) and self.player.sprite.direction.x < 0:
+            self.map_shift = self.player.sprite.player_speed
+            self.player.sprite.direction.x = 0
+        else:
+            self.map_shift = 0
+        
     def run(self):
+        # map
+        self.scroll_map()
+
         # render tiles
         self.tile_sprites.update(self.map_shift)
         self.tile_sprites.draw(self.screen)
